765a0eb2c51cfd551c4e3fc9033f86d2cdeb9b8f
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2022-06-08 00:59:51 +0400

    Implemented get dividends.

---
+++ src/InstrumentsService.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace rocketfellows\TinkoffInvestV1InstrumentsRestClient;
+
+use rocketfellows\TinkoffInvestV1RestClient\Client;
+use rocketfellows\TinkoffInvestV1RestClient\exceptions\request\ClientException;
+use rocketfellows\TinkoffInvestV1RestClient\exceptions\request\HttpClientException;
+use rocketfellows\TinkoffInvestV1RestClient\exceptions\request\ServerException;
+
+class InstrumentsService implements GetDividendsInterface
+{
+    private const SERVICE_NAME = 'InstrumentsService';
+
+    private const SERVICE_METHOD_NAME_GET_DIVIDENDS = 'GetDividends';
+
+    private $client;
+
+    public function __construct(Client $client)
+    {
+        $this->client = $client;
+    }
+
+    /**
+     * @throws ClientException
+     * @throws HttpClientException
+     * @throws ServerException
+     */
+    public function getDividends(array $params): array
+    {
+        return $this->client->request(
+            self::SERVICE_NAME,
+            self::SERVICE_METHOD_NAME_GET_DIVIDENDS,
+            $params
+        );
+    }
+}
