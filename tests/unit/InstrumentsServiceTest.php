4e4909e2c3d114e2fc4d4104f7dad346f9728986
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2022-06-08 00:41:05 +0400

    Added abstract test.

--- tests/unit/InstrumentsServiceTest.php
+++ tests/unit/InstrumentsServiceTest.php
@@ -2,12 +2,13 @@
 
 namespace rocketfellows\TinkoffInvestV1InstrumentsRestClient\tests\unit;
 
+use PHPUnit\Framework\MockObject\Builder\InvocationMocker;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 use rocketfellows\TinkoffInvestV1InstrumentsRestClient\InstrumentsService;
 use rocketfellows\TinkoffInvestV1RestClient\Client;
 
-class InstrumentsServiceTest extends TestCase
+abstract class InstrumentsServiceTest extends TestCase
 {
     /**
      * @var InstrumentsService
@@ -26,5 +27,32 @@ class InstrumentsServiceTest extends TestCase
         $this->client = $this->createMock(Client::class);
 
         $this->instrumentsService = new InstrumentsService($this->client);
+    }
+
+    abstract public function testMethodRequest(): void;
+
+    protected function assertClientRequestWithParams(
+        string $serviceName,
+        string $serviceMethod,
+        array $params,
+        ?array $response = []
+    ): void {
+        $this->assertClientRequest($response)->with($serviceName, $serviceMethod, $params);
+    }
+
+    protected function assertClientRequestWithoutParams(
+        string $serviceName,
+        string $serviceMethod,
+        ?array $response = []
+    ): void {
+        $this->assertClientRequest($response)->with($serviceName, $serviceMethod);
+    }
+
+    private function assertClientRequest(?array $response = []): InvocationMocker
+    {
+        return $this->client
+            ->expects($this->once())
+            ->method('request')
+            ->willReturn($response);
     }
 }
