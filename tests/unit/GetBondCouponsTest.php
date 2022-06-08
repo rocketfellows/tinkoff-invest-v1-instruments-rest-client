8de328e5fa8e7b10032715ec95ea0604eea3f72e
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2022-06-09 01:01:34 +0400

    Covered bond coupons getter.

---
+++ tests/unit/GetBondCouponsTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace rocketfellows\TinkoffInvestV1InstrumentsRestClient\tests\unit;
+
+use rocketfellows\TinkoffInvestV1InstrumentsRestClient\GetBondCouponsInterface;
+
+class GetBondCouponsTest extends InstrumentsServiceTest
+{
+    private const PARAMS = ['foo'];
+
+    protected function prepareServiceMethodCallAssertions(array $expectedResponse): array
+    {
+        $this->assertClientRequestWithParams('GetBondCoupons', self::PARAMS, $expectedResponse);
+
+        return $this->instrumentsService->getBondCoupons(self::PARAMS);
+    }
+
+    protected function getExpectedInterfacesImplementations(): array
+    {
+        return [GetBondCouponsInterface::class];
+    }
+}
