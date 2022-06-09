2162c8bf15b6d524088367af44e5b8ad22cb5234
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2022-06-10 00:57:18 +0400

    Added interface exceptions.

--- src/FindInstrumentInterface.php
+++ src/FindInstrumentInterface.php
@@ -2,7 +2,16 @@
 
 namespace rocketfellows\TinkoffInvestV1InstrumentsRestClient;
 
+use rocketfellows\TinkoffInvestV1RestClient\exceptions\request\ClientException;
+use rocketfellows\TinkoffInvestV1RestClient\exceptions\request\HttpClientException;
+use rocketfellows\TinkoffInvestV1RestClient\exceptions\request\ServerException;
+
 interface FindInstrumentInterface
 {
+    /**
+     * @throws ClientException
+     * @throws ServerException
+     * @throws HttpClientException
+     */
     public function findInstrument(array $params): array;
 }
