6adb002f447d9159166c3ba7ff1a3e34996c915b
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2022-06-11 00:27:25 +0400

    Cleaned.

--- src/InstrumentsService.php
+++ src/InstrumentsService.php
@@ -3,9 +3,6 @@
 namespace rocketfellows\TinkoffInvestV1InstrumentsRestClient;
 
 use rocketfellows\TinkoffInvestV1RestClient\Client;
-use rocketfellows\TinkoffInvestV1RestClient\exceptions\request\ClientException;
-use rocketfellows\TinkoffInvestV1RestClient\exceptions\request\HttpClientException;
-use rocketfellows\TinkoffInvestV1RestClient\exceptions\request\ServerException;
 
 class InstrumentsService implements
     GetDividendsInterface,
